2454fc4df8228882812d04d61ace63b94785ee00
tomzy-ba <trbee100@gmail.com>
2024-03-07 20:38:22 +0000

    finished part 4

--- app.js
+++ app.js
@@ -26,8 +26,15 @@ const ExampleSchema = new Schema({
 const SomeModel = mongoose.model("SomeModel", ExampleSchema);
 
 
-var app = express();
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 const catalogRouter = require("./routes/catalog");
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/catalog", catalogRouter);
+
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -38,7 +45,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use("/catalog/", catalogRouter);
 
 
 // catch 404 and forward to error handler
